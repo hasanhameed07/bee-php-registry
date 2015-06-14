291bb3feca3dbbff7235c3ff04d7b59e2dacf4b4
Hasan Hameed <hasanhameed89@gmail.com>
2015-06-14 14:59:25 +0500

    lib

---
+++ Bee_Registry.php
@@ -0,0 +1,76 @@
+<?PHP
+/**
+ * A Registry pattern used to recieve singleton instance of any object
+ */
+class Bee_Registry
+{
+
+	/**
+	 * _loaded - Loaded objects
+	 * @var array
+	 * @static
+	 */	
+	private static $_loaded = array();
+
+
+
+	// --- Public static methods ---
+
+	/**
+	 * Checks if an object is loaded in registry
+	 *
+	 * @param string	$name [obj name]
+	 * @return boolean
+	 */
+	public static function has ($name)
+	{
+		return (array_key_exists($name, self::$_loaded))? 1 : 0 ;
+	}
+
+
+	/**
+	 * Recieve an already loaded object from registry.
+	 *
+	 * @param string	$name [obj name]
+	 * @return obj		[only if loaded else throw exception]
+	 */
+	public static function &get ($name)
+	{
+		if (self::has($name))
+			return self::$_loaded[$name];
+		else
+			Bee_Exceptiom::throwE('Unable to get '.$name.' object from registry.');
+	}
+
+
+	/**
+	 * Stores new object in registry.
+	 *
+	 * @param obj		&$obj [object]
+	 * @param string	$name [optional name for object otherwise object's class name is used]
+	 */
+	public static function set (&$obj, $name=NULL)
+	{
+		$name = (is_null($name))? get_class($obj) : $name ;
+		self::$_loaded[$name] = $obj;
+	}
+
+
+	/**
+	 * Get a single instance of any object
+	 *
+	 * @param string	$name [class name of object]
+	 * @return obj		[singleton instance]
+	 */
+	public static function getInstance ($name)
+	{
+		if (self::has($name))
+			return self::get($name);
+
+		$obj = new $name();
+		self::set($obj, $name);
+		return $obj;
+	}
+
+} // Bee_Registry end
+?>
---
+++ README.md
@@ -0,0 +1,34 @@
+#Bee-PHP-Registry
+A Registry pattern used to recieve singleton instance of any object. This class is design to store and retrieve objects on demand.
+
+#### Registry::getInstance($className)
+Use this method to get a single instance of any class every time you call.
+Example:
+```php
+$singletonFoo = Registry::getInstance('Foo');
+```
+
+#### Registry::set(&$obj, $name=NULL)
+Stores new object in registry.
+Example:
+```php
+Registry::set($objFoo, 'Foo');
+```
+
+#### Registry::get($name)
+Recieve an already loaded object from registry. 
+(Note: It returns object by reference.)
+Example:
+```php
+$objFoo = Registry::get('Foo');
+```
+
+#### Registry::has($name)
+Checks if an object is loaded in registry.
+Example:
+```php
+if (Registry::has('Foo'))
+	// do something
+```
+
+Origin Author: Hasan Hameed <hasanhameed89@gmail.com>
